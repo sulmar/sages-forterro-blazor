442b0dd6a32a3e0e969da04ed502af674cc32ebf
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-13 19:15:39 +0200

    Usunięcie warnings

--- src/Domain/Models/Customer.cs
+++ src/Domain/Models/Customer.cs
@@ -2,9 +2,9 @@
 
 public class Customer : BaseEntity
 {
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string HashedPassword { get; set; }
-    public string ConfirmedPassword { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public string? HashedPassword { get; set; }
+    public string? ConfirmedPassword { get; set; }
     public bool IsDeleted { get; set; }
 }
--- src/Domain/Models/Product.cs
+++ src/Domain/Models/Product.cs
@@ -2,7 +2,7 @@
 
 public class Product : BaseEntity
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
     public decimal Price { get; set; }
 }
