efee2f548b860b391161076cffd2f4ba9720e59f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-14 14:10:03 +0200

    Refaktor: Przeniesienie logiki pobierania danych do partial class

---
+++ src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Abstractions;
+using Domain.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebAssemblyApp.Pages.Customers;
+
+public partial class List
+{
+    private string message = $"Lorem ipsum {DateTime.Now}";
+
+    private IEnumerable<Customer>? customers;
+
+    [Inject] 
+    private ICustomerRepository Repository { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        customers = await Repository.GetAllAsync();
+    }
+}
