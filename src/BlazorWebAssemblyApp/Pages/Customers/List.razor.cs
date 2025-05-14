682b146d6ab5b76527179d77a443ea9501084aa8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-14 14:12:04 +0200

    Refaktor: Dodanie wstrzykiwania zależności poprzez konstruktor

--- src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
+++ src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
@@ -1,20 +1,21 @@
 ﻿using Domain.Abstractions;
 using Domain.Models;
-using Microsoft.AspNetCore.Components;
 
 namespace BlazorWebAssemblyApp.Pages.Customers;
 
 public partial class List
 {
     private string message = $"Lorem ipsum {DateTime.Now}";
-
     private IEnumerable<Customer>? customers;
 
-    [Inject] 
-    private ICustomerRepository Repository { get; set; }
+    private readonly ICustomerRepository repository;
+    public List(ICustomerRepository repository)
+    {
+        this.repository = repository;
+    }
 
     protected override async Task OnInitializedAsync()
     {
-        customers = await Repository.GetAllAsync();
+        customers = await repository.GetAllAsync();
     }
 }
