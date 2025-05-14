fc02e4ec4911390b5ac8fb0aecd1e0736840a8cf
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-14 13:05:33 +0200

    Fix: Usunięcie wymaganego pola ze względu na Bogus

--- src/Domain/Models/Customer.cs
+++ src/Domain/Models/Customer.cs
@@ -2,8 +2,8 @@
 
 public class Customer : BaseEntity
 {
-    public required string Name { get; set; }
-    public required string Email { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
     public string? HashedPassword { get; set; }
     public string? ConfirmedPassword { get; set; }
     public bool IsDeleted { get; set; }
