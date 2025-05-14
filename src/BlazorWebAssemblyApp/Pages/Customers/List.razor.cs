f03dadaf92f39aa9facf6c63a3f20ac935d42a7d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-14 14:27:27 +0200

    Refaktor: Dodanie komponentu Title

--- src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
+++ src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
@@ -6,6 +6,7 @@ namespace BlazorWebAssemblyApp.Pages.Customers;
 public partial class List(ICustomerRepository repository) // Primary Constructor
 {
     private string message = $"Lorem ipsum {DateTime.Now}";
+
     private IEnumerable<Customer>? customers;
 
     protected override async Task OnInitializedAsync()
