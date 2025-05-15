02efaee49300416e7a9b8405d6dee0b3aca534d6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-15 11:28:54 +0200

    Refaktor: Zastosowanie złożonego obiektu w parametrach kaskadowych

---
+++ src/BlazorWebAssemblyApp/Models/Info.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWebAssemblyApp.Models;
+
+public class Info
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Count { get; set; }
+}
