580f446febf59d5e6b4f3c39f3ae58c12e1439de
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-16 09:49:36 +0200

    Zastosowanie walidacji za pomocą biblioteki FluentValidation

--- src/Domain/Models/Customer.cs
+++ src/Domain/Models/Customer.cs
@@ -1,17 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Domain.Models;
+﻿namespace Domain.Models;
 
 public class Customer : BaseEntity
 {
-    [Required, MinLength(1), MaxLength(10)]
     public string Name { get; set; }
-    [EmailAddress]
     public string Email { get; set; }
-    [Range(-1000, 1000)]
     public decimal Balance { get; set; }
     public string? HashedPassword { get; set; }
-    [Compare(nameof(HashedPassword))]
     public string? ConfirmedPassword { get; set; }
     public bool IsDeleted { get; set; }
 }
---
+++ src/Domain/Validators/CustomerValidator.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace Domain.Validators;
+
+// dotnet add package FluentValidation
+public class CustomerValidator : AbstractValidator<Customer>
+{
+    public CustomerValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty()
+            .MinimumLength(1)
+            .MaximumLength(10);
+
+        RuleFor(p => p.Email)
+            .EmailAddress();
+
+        RuleFor(p => p.Balance)
+            .InclusiveBetween(-1000, 1000);
+
+        RuleFor(p => p.ConfirmedPassword)
+            .Equal(p => p.HashedPassword);
+    }
+}
